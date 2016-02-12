116f545be9f021e819f24bc43c5547783d2bd67d
Alana Farkas <alana.farkas@gmail.com>
2016-02-11 19:55:47 -0500

    complete leap year files

---
+++ week-4/leap-years/my_solution.rb
@@ -0,0 +1,21 @@
+# Leap Years
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def leap_year?(year)
+	if year % 4 == 0 && year % 100 != 0
+		return true
+	elsif year % 400 == 0
+		return true
+	else
+		return false
+	end
+end
+
+puts "Hey is your year a leap year? Input to find out!"
+year = gets.chomp.to_i
+
+leap_year?(year)
