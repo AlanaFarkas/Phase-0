4250001df68461d4d3c8d6fa37389d9eb93ae087
Alana Farkas <alana.farkas@gmail.com>
2016-02-18 22:41:19 -0500

    some working code, pseudocode

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -3,34 +3,53 @@
 # I spent [] hours on this challenge.
 
 # Write a method separate_comma which takes a positive integer as its input and returns a comma-separated integer as a string.
+# Write a method separate_comma which takes a positive integer as its input and returns a comma-separated integer as a string. 
 
 # 0. Pseudocode
 
-# What is the input?
-#-Integer
-# What is the output? (i.e. What should the code return?)
-#-String
+# What is the input? Integer
+# What is the output? String
 # What are the steps needed to solve the problem?
-First I should convert the integer to a string
-Then I convert the string into an array of strings
-Then I join the array to become one string, with a comma placed every 3rd index 
 
+# Create comma-separated method that accepts an array
+# Set the array to a string
+# Split the array
+# If the array.length < 4 return the array as a string
 
-# 1. Initial Solution
-	
-def seperate_comma(integer)
-  array = Array.new
+# If integer.length > 3, insert comma[-1] and counter = 0
+# For every three indexes, counter therafter, add another comma
+#numbers get added to the front of the array
+# Reverse is last step?
+
   
-  integer = integer.to_s  
-  array = integer.split(//)
-  p array
+def separate_comma(integer)
   
-  # puts array.values_at(-3)
+  integer = integer.to_s
+  counter = 0 
   
-  # p array.join("")
- 
+  if integer.length < 4
+    p integer
+  end
+  
+  if integer.length > 3
+    integer.reverse
+    number_as_string = []
+    counter = 1
+    
+  end
+  
+#  integer = integer.to_s.reverse
+  integer.insert(-3, ",") 
+   
+#   p integer
+    
+#   end
+  p integer
  
 end
+
+separate_comma(100000)
+
 
 
 
