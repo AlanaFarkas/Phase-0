97d6ec134ebd787ec5a1c384e1d1202a9059c179
Alana Farkas <alana.farkas@gmail.com>
2016-02-21 20:46:35 -0500

    working code, no refactor, no reflection

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -1,8 +1,9 @@
 # Numbers to Commas Solo Challenge
 
-# I spent [] hours on this challenge.
+# I spent 6 hours on this challenge.
 
 # Write a method separate_comma which takes a positive integer as its input and returns a comma-separated integer as a string. 
+
 
 # 0. Pseudocode
 
@@ -10,42 +11,50 @@
 # What is the output? String
 # What are the steps needed to solve the problem?
 
-# Create comma-separated method that accepts an array
-# Set the array to a string
-# Split the array
-# If the array.length < 4 return the array as a string
+# Convert the number into a string
+# IF the string.length < 4 RETURN the string
 
-# If integer.length > 3, insert comma[-1] and counter = 0
-# For every three indexes, counter therafter, add another comma
-#numbers get added to the front of the array
-# Reverse is last step?
+# Set forumla by which to determine how many commas are needed in a number (string.leng) 
+# If string.length > 3, insert comma at index[-4]
+#
 
-# def separate_comma(number)
-  
-#   number = number.to_s  
-   
-#   if number.length < 4
-#     return number
-#   end
+# p 1000000.to_s.length / 3
+# p 1000000.to_s.length % 3
+
+# total_commas_needed = 3
+
+# counter = -4
+# total_commas_needed.times do
+#   puts "hello #{counter}"
+#   counter -= 4
+# end
+
+#   number.insert(-4, ",")
+#   number.insert(-8, ",")
+#   number.insert(-12, ",")
+#   number.insert(-16, ",") 
     
-#   if number.length > 3
-    
-#     total_commas_needed = number.to_s.length / 3 
-
 #     counter = -4
-#     total_commas_needed.times do
-      
+#     total_commas_needed.times do      
 #       number.insert(counter, ",")
-#       counter -= 4
-      
-#     end
-    
-   
-#     return number    
-    
- 
-# end
-# end
+#       counter -= 4      
+#   end
+
+
+
+#WORKS FOR 6 NUMBERS!!
+  # if number.length > 3 && number.length % 3 == 0  
+  #   total_commas_needed = total_commas_needed-1
+  #           #100000
+  #   counter = -4
+  #   total_commas_needed.times do      
+  #     number.insert(counter, ",")
+  #     counter -= 4     
+  #   end
+  # end  
+
+
+
 def separate_comma(number)
   
   number = number.to_s 
@@ -53,21 +62,23 @@ def separate_comma(number)
    
   if number.length < 4
     return number
-  end 
-
-    
+  end     
   
-  if number.length > 3 && number.length % 3 == 0  
-    total_commas_needed = total_commas_needed-1
-            
+  if number.length > 3 && number.length % 3 == 0 
+    # 6 digits => 2 commas but need only 1
+    
+    total_commas_needed = total_commas_needed -1 
+    p total_commas_needed
+    
     counter = -4
-    total_commas_needed.times do      
+    total_commas_needed.times do          
       number.insert(counter, ",")
-      counter -= 4     
+      counter = -4
+     return number
     end
   end 
    
-if number.length > 3        
+if number.length > 3 
     counter = -4
     total_commas_needed.times do      
       number.insert(counter, ",")
@@ -79,7 +90,7 @@ if number.length > 3
     return number
 end
 
-
+# 
 
 
 # 2. Refactored Solution
@@ -88,3 +99,17 @@ end
 
 
 # 3. Reflection
+# What was your process for breaking the problem down? What different approaches did you consider?
+# 
+
+# Was your pseudocode effective in helping you build a successful initial solution?
+# What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them (any difficulties, etc.). Did it/they significantly change the way your code works? If so, how?
+# How did you initially iterate through the data structure?
+# Do you feel your refactored solution is more readable than your initial solution? Why?
+
+
+
+
+
+
+
